ad238e09af44ce82ce511308d60e0ea4ab83cda6
Neco Kriel <necokriel@gmail.com>
2025-03-08 09:48:44 +1100

    add: smoothing jpdf option

--- src/Loki/WWStats/ComputePDFs.py
+++ src/Loki/WWStats/ComputePDFs.py
@@ -6,6 +6,7 @@
 ## ###############################################################
 import numpy
 from Loki.Utils import Utils4Funcs
+from Loki.WWStats import SimpleStats
 
 
 ## ###############################################################
@@ -64,6 +65,36 @@ def computeJPDF(
   jpdf = bin_counts / (numpy.sum(bin_counts) * bin_area)
   return bedges_x, bedges_y, jpdf
 
+def computeJPDF(
+  data_x              : numpy.ndarray,
+  data_y              : numpy.ndarray,
+  bedges_x            : numpy.ndarray = None,
+  bedges_y            : numpy.ndarray = None,
+  num_bins            : int = None,
+  weights             : numpy.ndarray = None,
+  bedge_extend_factor : float = 0.0,
+  smoothing_length    : float = None,
+  ):
+  """Compute the 2D joint probability density function (JPDF)."""
+  if (len(data_x) == 0) or (len(data_y) == 0):
+    raise ValueError("Error: Data arrays must not be empty.")
+  if (bedges_x is None) and (bedges_y is None) and (num_bins is None):
+    raise ValueError("Error: You did not provide a binning option.")
+  if bedges_x is None: bedges_x = compute1DBins(data_x, num_bins, bedge_extend_factor)
+  if bedges_y is None: bedges_y = compute1DBins(data_y, num_bins, bedge_extend_factor)
+  bin_counts    = numpy.zeros((len(bedges_x) - 1, len(bedges_y) - 1), dtype=float)
+  bin_indices_x = numpy.clip(numpy.searchsorted(bedges_x, data_x, side="right") - 1, 0, len(bedges_x) - 2)
+  bin_indices_y = numpy.clip(numpy.searchsorted(bedges_y, data_y, side="right") - 1, 0, len(bedges_y) - 2)
+  if weights is not None:
+    if weights.size != data_x.size:
+      raise ValueError("Error: The size of weights must match the size of data arrays.")
+    numpy.add.at(bin_counts, (bin_indices_x, bin_indices_y), weights)
+  else: numpy.add.at(bin_counts, (bin_indices_x, bin_indices_y), 1)
+  bin_area = numpy.abs((bedges_x[1] - bedges_x[0]) * (bedges_y[1] - bedges_y[0]))
+  jpdf = bin_counts / (numpy.sum(bin_counts) * bin_area)
+  if smoothing_length is not None: jpdf = SimpleStats.smoothWithGaussianFilter(jpdf, smoothing_length)
+  return bedges_x, bedges_y, jpdf
+
 @Utils4Funcs.time_function
 def compute1DPDF(
     data     : numpy.ndarray,
@@ -99,7 +130,6 @@ def compute1DBins(
     stop  = data_p84 + (2 + extend_factor) * (data_p84 - data_p50),
     num   = int(num_bins)
   )
-
 
 
 ## END OF MODULE
--- tests/test_ComputeJPDFs.py
+++ tests/test_ComputeJPDFs.py
@@ -9,9 +9,11 @@ from Loki.WWPlots import PlotUtils
 ## ###############################################################
 ## HELPER FUNCTIONS
 ## ###############################################################
-def generate_ellipse_samples(num_samples):
-  center_x, center_y = 0, 0
-  semi_major_axis, semi_minor_axis = 4, 2
+def sampleFromEllipse(num_samples):
+  center_x = 0
+  center_y = 0
+  semi_major_axis = 4
+  semi_minor_axis = 2
   rotation_angle_rad = numpy.pi / 4
   x_samples = numpy.random.normal(center_x, semi_major_axis, int(num_samples))
   y_samples = numpy.random.normal(center_y, semi_minor_axis, int(num_samples))
@@ -25,12 +27,12 @@ def generate_ellipse_samples(num_samples):
 ## ###############################################################
 def main():
   num_points = 1e5
-  num_bins   = 100
-  x_samples, y_samples = generate_ellipse_samples(num_points)
-  bedges_x, bedges_y, jpdf = ComputePDFs.computeJPDF(x_samples, y_samples, num_bins=num_bins)
+  num_bins   = 1e2
+  x_samples, y_samples = sampleFromEllipse(num_points)
+  bedges_x, bedges_y, jpdf = ComputePDFs.computeJPDF(x_samples, y_samples, num_bins=num_bins, smoothing_length=2.0)
   fig, ax = PlotUtils.initFigure(num_cols=1)
   ax.contourf(bedges_x[:-1], bedges_y[:-1], jpdf.T, levels=20, cmap="Blues")
-  # ax.scatter(x_samples, y_samples, color="red", s=10, alpha=1e-1)
+  ax.scatter(x_samples, y_samples, color="red", s=3, alpha=1e-2)
   ax.set_xlim([ numpy.min(bedges_x), numpy.max(bedges_x) ])
   ax.set_ylim([ numpy.min(bedges_y), numpy.max(bedges_y) ])
   PlotUtils.saveFigure(fig, "test_ComputeJPDFs.png")
