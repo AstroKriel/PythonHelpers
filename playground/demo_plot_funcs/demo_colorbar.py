49641e3105d61188c9b7c15a678d8eada28c556b
Neco Kriel <necokriel@gmail.com>
2025-04-02 22:34:16 +1100

    update: move colorbar func to library

--- playground/demo_plot_funcs/demo_colorbar.py
+++ playground/demo_plot_funcs/demo_colorbar.py
@@ -2,42 +2,7 @@
 ## DEPENDANCIES
 ## ###############################################################
 import numpy
-import matplotlib.ticker as mpl_ticker
 from loki.ww_plots import plot_manager, plot_color
-
-
-def add_cbar_from_cmap(
-    ax, cmap, norm,
-    label         : str = "",
-    side          : str = "right",
-    percentage    : float = 0.1,
-    cbar_padding  : float = 0.02,
-    label_padding : float = 10,
-    fontsize      : float = 20,
-  ):
-  fig = ax.figure
-  box = ax.get_position()
-  if side in [ "left", "right" ]:
-    orientation = "vertical"
-    cbar_size = box.width * percentage
-    if side == "right": cbar_bounds = [ box.x1 + cbar_padding, box.y0, cbar_size, box.height ]
-    else:               cbar_bounds = [ box.x0 - cbar_size - cbar_padding, box.y0, cbar_size, box.height ]
-  elif side in [ "top", "bottom" ]:
-    orientation = "horizontal"
-    cbar_size = box.height * percentage
-    if side == "top": cbar_bounds = [ box.x0, box.y1 + cbar_padding, box.width, cbar_size ]
-    else:             cbar_bounds = [ box.x0, box.y0 - cbar_size - cbar_padding, box.width, cbar_size ]
-  else: raise ValueError(f"Unsupported side: {side}")
-  ax_cbar = fig.add_axes(cbar_bounds)
-  cbar = fig.colorbar(mappable=None, cmap=cmap, norm=norm, cax=ax_cbar, orientation=orientation)
-  if orientation == "horizontal":
-    cbar.ax.set_title(label, fontsize=fontsize, pad=label_padding)
-    cbar.ax.xaxis.set_ticks_position(side)
-  else:
-    cbar.set_label(label, fontsize=fontsize, rotation=-90, va="bottom")
-    cbar.ax.yaxis.set_ticks_position(side)
-  return cbar
-
 
 
 ## ###############################################################
@@ -73,7 +38,7 @@ def main():
   ax.set_aspect("equal")
   ax.set_xlim([axis_bounds[0], axis_bounds[1]])
   ax.set_ylim([axis_bounds[2], axis_bounds[3]])
-  add_cbar_from_cmap(
+  plot_color.add_cbar_from_cmap(
     ax    = ax,
     cmap  = cmap,
     norm  = norm,
