acb02f42167d539436ae319693ed7b0cc0598e90
Neco Kriel <necokriel@gmail.com>
2025-04-10 15:05:57 +1000

    add test for casting lists to string

--- tests/utils/test_list_utils.py
+++ tests/utils/test_list_utils.py
@@ -11,43 +11,67 @@ from loki.utils import list_utils
 ## ###############################################################
 class TestListUtils(unittest.TestCase):
 
-  def test_getIntersectOfLists(self):
-    ## typical case: intersection of two lists
+  def test_cast_to_string(self):
+    result = list_utils.cast_to_string(["a", "b", "c"])
+    self.assertEqual(result, "`a`, `b`, or `c`")
+    ## with conjunction but no Oxford comma
+    result = list_utils.cast_to_string(["a", "b", "c"], conjunction="and", use_oxford_comma=False)
+    self.assertEqual(result, "`a`, `b` and `c`")
+    ## with conjunction and Oxford comma
+    result = list_utils.cast_to_string(["a", "b", "c"], conjunction="and", use_oxford_comma=True)
+    self.assertEqual(result, "`a`, `b`, and `c`")
+    ## with only two elements
+    result = list_utils.cast_to_string(["a", "b"])
+    self.assertEqual(result, "`a` or `b`")
+    result = list_utils.cast_to_string(["a", "b"], conjunction="")
+    self.assertEqual(result, "`a`, `b`")
+    ## single element
+    result = list_utils.cast_to_string(["a"])
+    self.assertEqual(result, "`a`")
+    ## empty list
+    result = list_utils.cast_to_string([])
+    self.assertEqual(result, "")
+    ## no quotes
+    result = list_utils.cast_to_string(["x", "y"], wrap_in_quotes=False, conjunction="and")
+    self.assertEqual(result, "x and y")
+
+  def test_get_intersect_of_lists(self):
+    ## intersection of two lists
     list_output = list_utils.get_intersect_of_lists([1, 2, 3], [2, 3, 4])
     list_expected = [2, 3]
     self.assertEqual(list_output, list_expected)
-    ## edge case 1: no intersection
+    ## no intersection
     list_output = list_utils.get_intersect_of_lists([1, 2, 3], [4, 5, 6])
     list_expected = []
     self.assertEqual(list_output, list_expected)
-    ## edge case 2: identical lists
+    ## identical lists
     list_output = list_utils.get_intersect_of_lists([1, 2, 3], [1, 2, 3])
     list_expected = [1, 2, 3]
     self.assertEqual(list_output, list_expected)
-    ## edge case 3: one list is empty
+    ## one list is empty
     list_output = list_utils.get_intersect_of_lists([], [1, 2, 3])
     list_expected = []
     self.assertEqual(list_output, list_expected)
 
-  def test_getUnionOfLists(self):
-    ## typical case: union of two lists
+  def test_get_union_of_lists(self):
+    ## union of two lists
     list_output = list_utils.get_union_of_lists([1, 2, 3], [2, 3, 4])
     list_expected = [1, 2, 3, 4]
     self.assertEqual(list_output, list_expected)
-    ## edge case 1: no common elements, just merge both lists
+    ## no common elements, just merge both lists
     list_output = list_utils.get_union_of_lists([1, 2, 3], [4, 5, 6])
     list_expected = [1, 2, 3, 4, 5, 6]
     self.assertEqual(list_output, list_expected)
-    ## edge case 2: identical lists, should return one set of values
+    ## identical lists, should return one set of values
     list_output = list_utils.get_union_of_lists([1, 2, 3], [1, 2, 3])
     list_expected = [1, 2, 3]
     self.assertEqual(list_output, list_expected)
-    ## edge case 3: one list is empty
+    ## one list is empty
     list_output = list_utils.get_union_of_lists([], [1, 2, 3])
     list_expected = [1, 2, 3]
     self.assertEqual(list_output, list_expected)
 
-  def test_getIndexOfClosestValue(self):
+  def test_get_index_of_closest_value(self):
     ## typical case 1: target value exists in the list
     output_index = list_utils.get_index_of_closest_value([1, 5, 8], 5)
     expected_index = 1
@@ -56,32 +80,32 @@ class TestListUtils(unittest.TestCase):
     output_index = list_utils.get_index_of_closest_value([1, 5, 8], 6)
     expected_index = 1
     self.assertEqual(output_index, expected_index)
-    ## edge case 1: target value is None
+    ## target value is None
     with self.assertRaises(Exception):
       list_utils.get_index_of_closest_value([1, 2, 3], None)
-    ## edge case 2: target value is infinity
+    ## target value is infinity
     output_index = list_utils.get_index_of_closest_value([1, 5, 8], numpy.inf)
     expected_index = 2
     self.assertEqual(output_index, expected_index)
-    ## edge case 3: target value is negative infinity
+    ## target value is negative infinity
     output_index = list_utils.get_index_of_closest_value([1, 5, 8], -numpy.inf)
     expected_index = 0
     self.assertEqual(output_index, expected_index)
 
-  def test_flattenList(self):
-    ## typical case: flatten a list of lists
+  def test_flatten_list(self):
+    ## flatten a list of lists
     list_output = list_utils.flatten_list([[1, 2], [3, 4], [5, 6]])
     list_expected = [1, 2, 3, 4, 5, 6]
     self.assertEqual(list_output, list_expected)
-    ## edge case 1: flatten an empty list of lists
+    ## flatten an empty list of lists
     list_output = list_utils.flatten_list([[], []])
     list_expected = []
     self.assertEqual(list_output, list_expected)
-    ## edge case 2: single list inside list, should return that list
+    ## single list inside list, should return that list
     list_output = list_utils.flatten_list([[1, 2, 3]])
     list_expected = [1, 2, 3]
     self.assertEqual(list_output, list_expected)
-    ## edge case 3: already a flat list, should return the same list
+    ## already a flat list, should return the same list
     list_output = list_utils.flatten_list([1, 2, 3])
     list_expected = [1, 2, 3]
     self.assertEqual(list_output, list_expected)
