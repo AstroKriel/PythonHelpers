b2ffb1a5e91560bcf0141eee6c682440dac308e7
Neco Kriel <necokriel@gmail.com>
2025-04-10 15:06:44 +1000

    add funcs to read flash vi + plot data

---
+++ src/loki/ww_io/flash_data/_read_grid_data.py
@@ -0,0 +1,98 @@
+## START OF MODULE
+
+
+## ###############################################################
+## DEPENDENCIES
+## ###############################################################
+import h5py
+import numpy
+from loki.ww_io import file_manager
+
+
+## ###############################################################
+## FUNCTIONS
+## ###############################################################
+def read_grid_properties(file_path):
+  file_manager.does_file_exist(file_path=file_path, raise_error=True)
+  def _extract_properties(_h5file, dataset_name):
+    return {
+      str(key).split("'")[1].strip(): value
+      for key, value in _h5file[dataset_name]
+    }
+  ## check that the file is the right type and has the right structure before proceeding
+  properties = {}
+  try:
+    ## read datasets
+    with h5py.File(file_path, "r") as h5file:
+      properties["plasma_datasets"] = [
+        dataset_name
+        for dataset_name in h5file.keys()
+        if any(
+          dataset_name.startswith(prefix)
+          for prefix in ("mag", "vel", "dens", "cur")
+        )
+      ]
+      properties["int_scalars"]    = _extract_properties(h5file, "integer scalars")
+      properties["int_properties"] = _extract_properties(h5file, "integer runtime parameters")
+  except KeyError as exception:
+    print(f"Error: The group {exception} was not found in: {file_path}.")
+    return {}
+  except Exception as exception:
+    print(f"An unexpected error occurred: {exception}")
+    return {}
+  if len(properties["plasma_datasets"]) == 0: print(f"Warning: No plasma datasets found in: {file_path}")
+  try:
+    output_num    = properties["int_scalars"]["plotfilenumber"]
+    dataset_names = properties["plasma_datasets"]
+    num_blocks    = numpy.int32(properties["int_scalars"]["globalnumblocks"])
+    num_blocks_x  = numpy.int32(properties["int_properties"]["iprocs"])
+    num_blocks_y  = numpy.int32(properties["int_properties"]["jprocs"])
+    num_blocks_z  = numpy.int32(properties["int_properties"]["kprocs"])
+    num_cells_per_block_x = numpy.int32(properties["int_scalars"]["nxb"])
+    num_cells_per_block_y = numpy.int32(properties["int_scalars"]["nyb"])
+    num_cells_per_block_z = numpy.int32(properties["int_scalars"]["nzb"])
+    num_cells_per_block   = num_cells_per_block_x * num_cells_per_block_y * num_cells_per_block_z
+    num_cells             = num_blocks * num_cells_per_block
+    return {
+      "output_num"            : output_num,
+      "dataset_names"         : dataset_names,
+      "num_blocks"            : num_blocks,
+      "num_blocks_x"          : num_blocks_x,
+      "num_blocks_y"          : num_blocks_y,
+      "num_blocks_z"          : num_blocks_z,
+      "num_cells_per_block_x" : num_cells_per_block_x,
+      "num_cells_per_block_y" : num_cells_per_block_y,
+      "num_cells_per_block_z" : num_cells_per_block_z,
+      "num_cells"             : num_cells,
+    }
+  except KeyError as missing_key:
+    print(f"Error: Missing key `{missing_key}` in the extracted properties from: {file_path}")
+    return {}
+
+def reformat_flash_sfield(
+    sfield              : numpy.ndarray,
+    num_blocks          : tuple[int, int, int],
+    num_cells_per_block : tuple[int, int, int],
+  ):
+  ## input Fortran-style field (column-major: z, y, x) with shape:
+  ## [total_number_of_blocks, num_cells_per_block_z, num_cells_per_block_y, num_cells_per_block_x]
+  ## where total_number_of_blocks = num_blocks_x * num_blocks_y * num_blocks_z
+  ## reshape this field to separate the unified block-structure into its individual block components:
+  ## total_number_of_blocks -> [num_blocks_z, num_blocks_y, num_blocks_x]
+  sfield = sfield.reshape(
+    num_blocks[2], num_blocks[1], num_blocks[0],
+    num_cells_per_block[2], num_cells_per_block[1], num_cells_per_block[0]
+  )
+  ## interleave blocks with their cells
+  sfield = numpy.transpose(sfield, (0, 3, 1, 4, 2, 5))
+  ## merge block and cell dimensions
+  sfield_sorted = sfield.reshape(
+    num_blocks[2] * num_cells_per_block[2],
+    num_blocks[1] * num_cells_per_block[1],
+    num_blocks[0] * num_cells_per_block[0],
+  )
+  ## convert from Fortran-style [z, y, x] to C-style [x, y, z] cell ordering
+  return sfield_sorted.transpose((2, 1, 0))
+
+
+## END OF MODULE
---
+++ src/loki/ww_io/flash_data/_read_vi_data.py
@@ -0,0 +1,87 @@
+## START OF MODULE
+
+
+## ###############################################################
+## DEPENDENCIES
+## ###############################################################
+import numpy
+from loki.utils import list_utils
+from loki.ww_io import file_manager
+
+
+## ###############################################################
+## FUNCTIONS
+## ###############################################################
+def load_vi_data(
+  directory     : str,
+  file_name     : str = "Turb.dat",
+  dataset_name  : str | None = None,
+  dataset_index : int | None = None,
+  time_norm     : float = 1.0,
+  time_start    : float = 0.0,
+  time_end      : float | None = None,
+  debug         : bool = False,
+  verbose       : bool = False,
+  print_header  : bool = False,
+) -> tuple[list[float], list[float]]:
+  def _print_header(header):
+    print(f"Available datasets in: {file_path}")
+    for _dataset_index, _dataset_name in enumerate(header.strip().split()):
+      print(f"\t> {_dataset_index:3d}: {_dataset_name}")
+  file_path = file_manager.create_file_path([directory, file_name])
+  file_manager.does_file_exist(file_path=file_path, raise_error=True)
+  with open(file_path, "r") as file:
+    file_lines = file.readlines()
+  header = file_lines[0].split()
+  is_new_format = "#01_time" in header
+  if not(is_new_format): print("looking at an old dataset.")
+  lookup_dataset_index = {
+    "kin"  : 9  if is_new_format else 6,
+    "mag"  : 11 if is_new_format else 29,
+    "mach" : 13 if is_new_format else 8,
+  }
+  if print_header:
+    _print_header(header)
+    return [], []
+  if dataset_index is None:
+    if dataset_name is None: raise ValueError("Error: You need to either provide `dataset_index` or `dataset_name`.")
+    dataset_name = dataset_name.lower()
+    if dataset_name not in lookup_dataset_index:
+      valid_datasets = list_utils.cast_to_string(lookup_dataset_index.keys())
+      _print_header(header)
+      raise ValueError(f"Error: `{dataset_name}` is an invalid dataset. Choose from: {valid_datasets}, or provide `dataset_index` directly.")
+    dataset_index = lookup_dataset_index[dataset_name]
+  time_index   = 0 # time-values should always be stored in the first column-index
+  prev_time    = numpy.inf
+  num_datasets = len(file_lines[0].split()) # each header entry is a quantity
+  times        = []
+  values       = []
+  ## read backwards to prioritise the latest instance of a data-point (e.g., when the sim has been restarted)
+  for line in reversed(file_lines[1:]):
+    datasets = line.strip().split()
+    if len(datasets) != num_datasets: continue
+    if "#" in datasets[time_index] or "#" in datasets[dataset_index]: continue
+    try:
+      time_value    = float(datasets[time_index]) / time_norm
+      dataset_value = float(datasets[dataset_index])
+    except ValueError: continue
+    ## only add data at times that maintain monotonicity in time values
+    if time_value < prev_time:
+      if (dataset_value == 0.0) and (time_value > 0):
+        message = (f"{file_name}: field[{dataset_index:d}] = 0.0 at time = {time_value:.3f}")
+        if debug: raise ValueError(f"Error: {message}")
+        if verbose: print(f"Warning: {message}")
+        continue
+      times.append(time_value)
+      values.append(dataset_value)
+      prev_time = time_value
+  times.reverse()
+  values.reverse()
+  time_end    = time_end if time_end is not None else times[-1]
+  start_index = list_utils.get_index_of_closest_value(times, time_start)
+  end_index   = list_utils.get_index_of_closest_value(times, time_end)
+  if start_index == end_index: end_index = min(end_index+1, len(times)) # avoid empty ranges
+  return times[start_index:end_index], values[start_index:end_index]
+
+
+## END OF MODULE
