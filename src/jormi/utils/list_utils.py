76cf931b0a32ddbf3ad040f3371af54d942a6330
Neco Kriel <necokriel@gmail.com>
2025-04-25 19:32:27 +1000

    add function to find crossings of a target value in a list

--- src/jormi/utils/list_utils.py
+++ src/jormi/utils/list_utils.py
@@ -11,6 +11,48 @@ from jormi.utils import var_utils
 ## ###############################################################
 ## FUNCTIONS
 ## ###############################################################
+def get_index_of_closest_value(
+    values: list | numpy.ndarray,
+    target: float,
+  ) -> int:
+  """Find the index of the closest value to a `target` value."""
+  var_utils.assert_type(values, (list, numpy.ndarray))
+  var_utils.assert_type(target, (int, float))
+  if len(values) == 0: raise ValueError("Input list cannot be empty")
+  array = numpy.asarray(values)
+  if target is None: return None
+  if target ==  numpy.inf: return int(numpy.nanargmax(array))
+  if target == -numpy.inf: return int(numpy.nanargmin(array))
+  return int(numpy.nanargmin(numpy.abs(array - target)))
+
+def find_first_crossing(
+    values : list[float] | numpy.ndarray,
+    target : float,
+    direction : str | None = None
+  ):
+  values = numpy.asarray(values)
+  min_value = numpy.min(values)
+  max_value = numpy.max(values)
+  if not (min_value <= target <= max_value):
+    raise ValueError(f"`target` ({target:.2f}) is outside the range of the input values: [{min_value:.2f}, {max_value:.2f}].")
+  valid_filters = [ "rising", "falling", None ]
+  if direction not in valid_filters:
+     raise ValueError(f"`direction` must be one of {valid_filters}, but got {direction!r}. Choose from {cast_to_string(valid_filters)}")
+  if target == min_value: return numpy.argmin(values)
+  if target == max_value: return numpy.argmax(values)
+  for value_index in range(len(values)-1):
+    value_left  = values[value_index]
+    value_right = values[value_index+1]
+    crossed_target_while_rising = (value_left < target <= value_right)
+    crossed_target_while_falling = (value_right < target <= value_left)
+    if (direction == "rising") and crossed_target_while_rising:
+      return value_index
+    elif (direction == "falling") and crossed_target_while_falling:
+      return value_index
+    elif (direction is None) and (crossed_target_while_rising or crossed_target_while_falling):
+      return value_index
+  return None
+
 def cast_to_string(
     elems            : list | numpy.ndarray,
     conjunction      : str = "or",
@@ -25,8 +67,9 @@ def cast_to_string(
   ]
   if (conjunction != "") and (len(elems) > 1):
     if len(elems) == 2: return f"{elems[0]} {conjunction} {elems[1]}"
-    separator = "," if use_oxford_comma else ""
-    return ", ".join(elems[:-1]) + f"{separator} {conjunction} {elems[-1]}"
+    if use_oxford_comma and len(elems) > 2:
+      return ", ".join(elems[:-1]) + f", {conjunction} {elems[-1]}"
+    else: return ", ".join(elems[:-1]) + f" {conjunction} {elems[-1]}"
   return ", ".join(elems)
 
 def get_intersect_of_lists(
@@ -52,20 +95,6 @@ def get_union_of_lists(
   if (len(list_a) == 0) or (len(list_b) == 0): return list(list_a) + list(list_b)
   set_union = set(list_a) | set(list_b)
   return sorted(set_union) if sort_values else list(set_union)
-
-def get_index_of_closest_value(
-    values: list | numpy.ndarray,
-    target: float,
-  ) -> int:
-  """Find the index of the closest value to a `target` value."""
-  var_utils.assert_type(values, (list, numpy.ndarray))
-  var_utils.assert_type(target, (int, float))
-  if len(values) == 0: raise ValueError("Input list cannot be empty")
-  array = numpy.asarray(values)
-  if target is None: return None
-  if target ==  numpy.inf: return int(numpy.nanargmax(array))
-  if target == -numpy.inf: return int(numpy.nanargmin(array))
-  return int(numpy.nanargmin(numpy.abs(array - target)))
 
 def flatten_list(elems : list | numpy.ndarray) -> list:
   """Flatten a nested list into a single list."""
