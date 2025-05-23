d78392c234a546c720d14cc1b010b76a4a88eae9
Neco Kriel <necokriel@gmail.com>
2025-05-23 16:20:22 +1000

    update variable names for consistency

--- src/jormi/ww_fields/derive_fields.py
+++ src/jormi/ww_fields/derive_fields.py
@@ -19,11 +19,11 @@ def compute_magnetic_curvature_terms(
     vbasis_normal  : numpy.ndarray,
     vbasis_tangent : numpy.ndarray,
     vfield_u       : numpy.ndarray,
-    box_width      : float = 1.0,
+    box_length     : float = 1.0,
     grad_order     : int = 2,
   ) -> tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
   ## du_j/dx_i: (component-j, gradient-direction-i, x, y, z)
-  r2tensor_grad_u = field_operators.compute_vfield_gradient(vfield_u, box_width, grad_order)
+  r2tensor_grad_u = field_operators.compute_vfield_gradient(vfield_u, box_length, grad_order)
   ## n_i n_j du_j/dx_i
   sfield_curvature = numpy.einsum("ixyz,jxyz,jixyz->xyz", vbasis_normal, vbasis_normal, r2tensor_grad_u)
   ## t_i t_j du_j/dx_i
@@ -35,12 +35,12 @@ def compute_magnetic_curvature_terms(
 @func_utils.time_function
 def compute_lorentz_force_terms(
     vfield_b   : numpy.ndarray,
-    box_width  : float = 1.0,
+    box_length : float = 1.0,
     grad_order : int = 2,
   ) -> tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]:
-  vbasis_tangent, vbasis_normal, _, sfield_kappa = decompose_fields.compute_tnb_terms(vfield_b, box_width, grad_order)
+  vbasis_tangent, vbasis_normal, _, sfield_kappa = decompose_fields.compute_tnb_terms(vfield_b, box_length, grad_order)
   sfield_sq_magn_b           = numpy.square(field_operators.compute_vfield_magnitude(vfield_b))
-  vfield_tot_grad_pressure   = 0.5 * field_operators.compute_sfield_gradient(sfield_sq_magn_b, box_width, grad_order)
+  vfield_tot_grad_pressure   = 0.5 * field_operators.compute_sfield_gradient(sfield_sq_magn_b, box_length, grad_order)
   vfield_align_grad_pressure = numpy.einsum("ixyz,jxyz,jxyz->ixyz", vbasis_tangent, vbasis_tangent, vfield_tot_grad_pressure)
   vfield_tension_force       = sfield_sq_magn_b * sfield_kappa * vbasis_normal
   vfield_ortho_grad_pressure = vfield_tot_grad_pressure - vfield_align_grad_pressure
