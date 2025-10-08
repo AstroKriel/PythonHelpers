c4ae0d20bdeb361d1e7422705e776c63286fc837
neco kriel <necokriel@gmail.com>
2025-10-08 15:33:38 +1100

    update var names

--- src/jormi/ww_data/finite_difference.py
+++ src/jormi/ww_data/finite_difference.py
@@ -12,45 +12,45 @@ import numpy
 
 
 def second_order_centered_difference(
-    sfield_q: numpy.ndarray,
+    sarray: numpy.ndarray,
     cell_width: float,
     grad_axis: int,
 ) -> numpy.ndarray:
     forward = -1
     backward = +1
-    q_p = numpy.roll(sfield_q, int(1 * forward), axis=grad_axis)
-    q_m = numpy.roll(sfield_q, int(1 * backward), axis=grad_axis)
-    return (q_p - q_m) / (2 * cell_width)
+    s_f = numpy.roll(sarray, int(1 * forward), axis=grad_axis)
+    s_b = numpy.roll(sarray, int(1 * backward), axis=grad_axis)
+    return (s_f - s_b) / (2 * cell_width)
 
 
 def fourth_order_centered_difference(
-    sfield_q: numpy.ndarray,
+    sarray: numpy.ndarray,
     cell_width: float,
     grad_axis: int,
 ) -> numpy.ndarray:
     forward = -1
     backward = +1
-    q_p1 = numpy.roll(sfield_q, int(1 * forward), axis=grad_axis)
-    q_p2 = numpy.roll(sfield_q, int(2 * forward), axis=grad_axis)
-    q_m1 = numpy.roll(sfield_q, int(1 * backward), axis=grad_axis)
-    q_m2 = numpy.roll(sfield_q, int(2 * backward), axis=grad_axis)
-    return (-q_p2 + 8 * q_p1 - 8 * q_m1 + q_m2) / (12 * cell_width)
+    s_f1 = numpy.roll(sarray, int(1 * forward), axis=grad_axis)
+    s_f2 = numpy.roll(sarray, int(2 * forward), axis=grad_axis)
+    s_b1 = numpy.roll(sarray, int(1 * backward), axis=grad_axis)
+    s_b2 = numpy.roll(sarray, int(2 * backward), axis=grad_axis)
+    return (-s_f2 + 8 * s_f1 - 8 * s_b1 + s_b2) / (12 * cell_width)
 
 
 def sixth_order_centered_difference(
-    sfield_q: numpy.ndarray,
+    sarray: numpy.ndarray,
     cell_width: float,
     grad_axis: int,
 ) -> numpy.ndarray:
     forward = -1
     backward = +1
-    q_p1 = numpy.roll(sfield_q, int(1 * forward), axis=grad_axis)
-    q_p2 = numpy.roll(sfield_q, int(2 * forward), axis=grad_axis)
-    q_p3 = numpy.roll(sfield_q, int(3 * forward), axis=grad_axis)
-    q_m1 = numpy.roll(sfield_q, int(1 * backward), axis=grad_axis)
-    q_m2 = numpy.roll(sfield_q, int(2 * backward), axis=grad_axis)
-    q_m3 = numpy.roll(sfield_q, int(3 * backward), axis=grad_axis)
-    return (q_p3 - 9 * q_p2 + 45 * q_p1 - 45 * q_m1 + 9 * q_m2 - q_m3) / (60 * cell_width)
+    s_f1 = numpy.roll(sarray, int(1 * forward), axis=grad_axis)
+    s_f2 = numpy.roll(sarray, int(2 * forward), axis=grad_axis)
+    s_f3 = numpy.roll(sarray, int(3 * forward), axis=grad_axis)
+    s_b1 = numpy.roll(sarray, int(1 * backward), axis=grad_axis)
+    s_b2 = numpy.roll(sarray, int(2 * backward), axis=grad_axis)
+    s_b3 = numpy.roll(sarray, int(3 * backward), axis=grad_axis)
+    return -(s_f3 - 9 * s_f2 + 45 * s_f1 - 45 * s_b1 + 9 * s_b2 - s_b3) / (60 * cell_width)
 
 
 ## } MODULE
