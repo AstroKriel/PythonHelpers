d54b03a3d5e23d3998bdc5cfa4fc6cd5fa52c152
neco kriel <necokriel@gmail.com>
2025-10-15 20:58:47 +1100

    update to return field type

--- src/jormi/ww_fields/generate_fields.py
+++ src/jormi/ww_fields/generate_fields.py
@@ -17,21 +17,21 @@ def generate_gaussian_random_sfield(
     correlation_length: float,
     sim_time: float | None = None,
 ) -> field_types.ScalarField:
-  """Generate a 3D random scalar field with Gaussian correlation length."""
-  white_noise = numpy.random.normal(0.0, 1.0, (num_cells, num_cells, num_cells))
-  ki_values = numpy.fft.fftfreq(num_cells)  # in cycles per grid length
-  kx_grid, ky_grid, kz_grid = numpy.meshgrid(ki_values, ki_values, ki_values, indexing="ij")
-  k_magn_grid = numpy.sqrt(kx_grid * kx_grid + ky_grid * ky_grid + kz_grid * kz_grid)
-  ## Gaussian filter in k-space with a particular lengthscale (correlation_length)
-  fft_filter = numpy.exp(-0.5 * numpy.square(numpy.multiply(k_magn_grid, correlation_length)))
-  fft_sarray = fft_filter * numpy.fft.fftn(white_noise)
-  sarray = numpy.fft.ifftn(fft_sarray).real
-  field_label = rf"$\exp(-(k\,\ell_\mathrm{{cor}})^2/2), \,\ell_\mathrm{{cor}}={correlation_length:.2f}$"
-  return field_types.ScalarField(
-      sim_time=sim_time,
-      data=sarray,
-      field_label=field_label,
-  )
+    """Generate a 3D random scalar field with Gaussian correlation length."""
+    white_noise = numpy.random.normal(0.0, 1.0, (num_cells, num_cells, num_cells))
+    ki_values = numpy.fft.fftfreq(num_cells)  # in cycles per grid length
+    kx_grid, ky_grid, kz_grid = numpy.meshgrid(ki_values, ki_values, ki_values, indexing="ij")
+    k_magn_grid = numpy.sqrt(kx_grid * kx_grid + ky_grid * ky_grid + kz_grid * kz_grid)
+    ## Gaussian filter in k-space with a particular lengthscale (correlation_length)
+    fft_filter = numpy.exp(-0.5 * numpy.square(numpy.multiply(k_magn_grid, correlation_length)))
+    fft_sarray = fft_filter * numpy.fft.fftn(white_noise)
+    sarray = numpy.fft.ifftn(fft_sarray).real
+    field_label = rf"$\exp(-(k\,\ell_\mathrm{{cor}})^2/2), \,\ell_\mathrm{{cor}}={correlation_length:.2f}$"
+    return field_types.ScalarField(
+        sim_time=sim_time,
+        data=sarray,
+        field_label=field_label,
+    )
 
 
 def generate_powerlaw_sfield(
@@ -66,14 +66,15 @@ def generate_powerlaw_sfield(
             numpy.power(k_perp_magn_grid, -alpha_perp / 2.0) *
             numpy.power(k_para_magn_grid, -alpha_para / 2.0)
         )
-    random_sarray = numpy.random.randn(num_cells, num_cells, num_cells) + 1j * numpy.random.randn(num_cells, num_cells, num_cells)
+    random_sarray = numpy.random.randn(num_cells, num_cells, num_cells)
+    random_sarray += 1j * numpy.random.randn(num_cells, num_cells, num_cells)
     fft_spectrum_sarray = random_sarray * amplitude
     spectrum_sarray = numpy.fft.ifftn(fft_spectrum_sarray).real
     return field_types.ScalarField(
-      sim_time=sim_time,
-      data=spectrum_sarray,
-      field_label=field_label,
-  )
+        sim_time=sim_time,
+        data=spectrum_sarray,
+        field_label=field_label,
+    )
 
 
 ## } MODULE
