f3d0c1c6d0cd334114198fb6b638db8dd2b2c2db
neco kriel <necokriel@gmail.com>
2026-03-03 14:12:41 +1100

    update: use uv-magn validator

--- src/jormi/ww_fields/fields_3d/field_type.py
+++ src/jormi/ww_fields/fields_3d/field_type.py
@@ -139,7 +139,7 @@ class VectorField_3D(_field_type.Field):
             fdata_param_name="<varray_3d>",
         )
 
-    def get_vcomp_sarray_3d(
+    def get_vcomp_3d_sarray(
         self,
         comp_axis: cartesian_axes.AxisLike_3D,
     ) -> numpy.ndarray:
@@ -171,23 +171,11 @@ class UnitVectorField_3D(VectorField_3D):
             vfield_3d=self,
             param_name="<uvfield_3d>",
         )
-        sarray_3d_vmagn_sq = _farray_operators.sum_of_varray_comps_squared(varray_3d=varray_3d)
-        if not numpy.all(numpy.isfinite(sarray_3d_vmagn_sq)):
-            raise ValueError("UnitVectorField_3D should not contain any NaN/Inf magnitudes.")
-        if numpy.any(sarray_3d_vmagn_sq <= 1e-300):
-            raise ValueError("UnitVectorField_3D should not contain any (near-)zero vectors.")
-        max_error = float(
-            numpy.max(
-                numpy.abs(
-                    numpy.sqrt(sarray_3d_vmagn_sq) - 1.0,
-                ),
-            ),
+        _farray_operators.ensure_uvarray_magnitude(
+            varray_3d=varray_3d,
+            tol=self.tol,
+            param_name="<uvfield_3d>",
         )
-        if max_error > self.tol:
-            raise ValueError(
-                "Vector magnitude deviates from unit-magnitude=1.0 by"
-                f" max(error)={max_error:.3e} (tol={self.tol}).",
-            )
 
     @classmethod
     def from_3d_vfield(
